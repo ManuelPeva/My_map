4c1e4b7a58213be90e223c04ab17292715167717
manuel_859 <manuel_859@outlook.es>
2023-11-15 12:56:18 -0600

    Solución de errores del archivo home.jsno recibia el contenido de las tarjetas informativas

--- JS/home.js
+++ JS/home.js
@@ -1,17 +1,21 @@
-document.addEventListener("DOMContentLoaded", function (){
+document.addEventListener("DOMContentLoaded", function () {
     var logoContainers = document.querySelectorAll('.logo-container');
 
-    logoContainers.forEach(function (container){
+    logoContainers.forEach(function (container) {
         container.addEventListener('click', function () {
             var targetId = container.getAttribute('data-target');
+            //Ejecución del codigo en consola para verificar su funcionamiento
+            console.log('Target ID: ',targetId)
             var cardContainer = document.getElementById(targetId);
+            //Ejecución de la escritura del elemento del ID
+            console.log('Card Container: ',cardContainer);
 
-            //Oculta todas las tarjetas
-            document.querySelectorAll('.card-container').forEach(function (card){
-                card.computedStyleMap.display = 'none';
+            // Oculta todas las tarjetas
+            document.querySelectorAll('.card-container').forEach(function (card) {
+                card.style.display = 'none';  // Corregir aquí
             });
 
-            //Muestra la tarjeta específica
+            // Muestra la tarjeta específica
             cardContainer.style.display = 'block';
         });
     });
