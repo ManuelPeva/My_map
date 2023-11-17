5bf4c4142d2f36eec041aecbebfe0beef20eee97
manuel_859 <manuel_859@outlook.es>
2023-11-17 11:08:05 -0600

    Terminación de las funciones de audio para cada objeto

--- JS/home.js
+++ JS/home.js
@@ -40,15 +40,63 @@ function changeLanguage(language){
             //acción predeterminada en ingles si el idioma no se reconoce
             sourceElement.src = './Assets/audio/english.mp3';
 }
-
     audioElement.load(); //Recarga el elemento de audio con la nueva fuente
 }
 
 //Creación de la segundo audio
+
 //Creación de la función de audio en el card
-function changeLanguageLuaHotel(language){
+function changeLanguageAbel(language){
+    var audioElement = document.getElementById('audioPlayer_Abel');
+    var sourceElement = document.getElementById('audioSource_Abel');
+
+    switch (language) {
+        case 'english':
+            sourceElement.src = './Assets/audio/luaHotel_spanish.wav';
+            break;
+        case 'maya':
+            sourceElement.src = './Assets/audio/maya.mp3';
+            break;
+        case 'german':
+            sourceElement.src = './Assets/audio/german.mp3';
+            break;
+        default:
+            //acción predeterminada en ingles si el idioma no se reconoce
+            sourceElement.src = './Assets/audio/english.mp3';
+}
+
+    audioElement.load(); //Recarga el elemento de audio con la nueva fuente
+}
+
+//Creación de la función de audio en el card Lua Hotel
+function changeLanguageLua(language){
     var audioElement = document.getElementById('audioPlayer_lua');
     var sourceElement = document.getElementById('audioSource_lua');
+
+    switch (language) {
+        case 'english':
+            sourceElement.src = './Assets/audio/luaHotel_spanish.wav';
+            break;
+        case 'maya':
+            sourceElement.src = './Assets/audio/maya.mp3';
+            break;
+        case 'german':
+            sourceElement.src = './Assets/audio/german.mp3';
+            break;
+        default:
+            //acción predeterminada en ingles si el idioma no se reconoce
+            sourceElement.src = './Assets/audio/english.mp3';
+}
+
+    audioElement.load(); //Recarga el elemento de audio con la nueva fuente
+}
+
+// ---------------
+
+//Creación de la función de audio en el card Lua Hotel
+function changeLanguageAak(language){
+    var audioElement = document.getElementById('audioPlayerAak');
+    var sourceElement = document.getElementById('audioSourceAak');
 
     switch (language) {
         case 'english':
