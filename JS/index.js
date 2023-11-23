920cbd19e13e1fab14f75bf90a571a9bb0bd2992
manuel_859 <manuel_859@outlook.es>
2023-11-23 13:02:17 -0600

    Corrección de la pantalla de carga del archivo index.html

--- JS/index.js
+++ JS/index.js
@@ -2,21 +2,25 @@ const bg = document.querySelector('.bg');
 const loadText = document.querySelector('.loading-text');
 
 
-let load=0;
+let load = 0;
 let interval = setInterval(blurring, 30)
 
 
-function blurring(){
-    load ++
-    if(load>99){clearInterval(interval);}
-    
+function blurring() {
+    load++
+    if (load > 99) {
+        clearInterval(interval);
+
+        //Creación de enlace a la página Home al llegar al 100%
+        window.location.href = 'home.html';
+    }
+
 
     loadText.innerText = `${load}%`
-    loadText.style.opacity =scale(load,0, 100,1,0)
-   
-    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
-function scale(number, inMin, inMax, outMin, outMax){
+function scale(number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
